bf895caf6c636769a9a6c2aef763ac555eb130cc
John Duval <duvaldinho@gmail.com>
2022-01-02 13:21:58 -0700

    added jest testing packages to package.json

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,4 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 test ('renders Hello World', () => {
