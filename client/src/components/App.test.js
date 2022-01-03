4c240963dc72f96d748e1abb344d51d381e9ce1a
John Duval <duvaldinho@gmail.com>
2022-01-03 11:40:37 -0700

    added test file

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
+// TESTING LIBRARY DOCS: https://testing-library.com/docs/react-testing-library/cheatsheet
+//                       https://github.com/testing-library/react-testing-library
+// JEST DOCS:            https://jestjs.io/docs/tutorial-react
+
 test ('renders Hello World', () => {
-    render(<App />);
-    const hello = screen.getByText(/Hello World/i);
-    expect(hello).toBeInTheDocument();
+    const { getByText } = render(<App />);
+
+    getByText('Hello World!')
+    // getByRole - find an element by role i.e. button or link
+        // ^ good for accessibility!
+
+
+    // TESTING A BUTTON CLICK EVENT     
+    // Click a button
+    // fireEvent.click(ELEMENT_HERE);
+    // expect(ELEMENT_HERE).toHaveStyle({ backgroundColor: 'blue });
+    
 })
